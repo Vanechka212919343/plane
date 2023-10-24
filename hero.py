e8342b1096cd5cf4559f89482f0d1c5fba9144a0
TatianaMaksakova <tatzianar@gmail.com>
2023-10-24 14:42:48 +0300

    update:corrable update:viskov

--- hero.py
+++ hero.py
@@ -1,12 +1,17 @@
 import pygame
 
-class hero():
-    def __init__(self):
+class Hero():
+    def __init__(self, screen):
         #This code snippet is defining a constructor (__init__) for a class. 
         # It calls the parent class's constructor using super().__init__().
         #  It then loads an image file called "hero.png" using the Pygame library and assigns it to the image attribute.
         #  Finally, it gets the rectangle of the image and assigns it to the rect attribute.
-        self.image = pygame.image.load()
+        self.image = pygame.image.load("images/hero.png")
         self.rect = self.image.get_rect()
         self.screen = screen
         self.screen_rect = screen.get_rect()
+        self.rect.bottom = self.screen_rect.bottom
+        self.rect.x = self.screen_rect.centerx
+
+    def output_hero(self):
+        self.screen.blit(self.image, self.rect)
--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 import pygame
 import sys
+from hero import Hero
+from pygame.locals import *
 
 def start_game():
     #основна функция для описания игры
     pygame.init()
     screen=pygame.display.set_mode((800,600))
     pygame.display.set_caption("Самая гут игра")
+    
+    #объекты классов
+    hero = Hero(screen)
 
+    flag = True
     flag = True
     while flag:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                flag=False
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    hero.rect.y -= 10
+                elif event.key == pygame.K_s:
+                    hero.rect.y += 10
+                elif event.key == pygame.K_a:
+                    hero.rect.x -= 10
+                elif event.key == pygame.K_d:
+                    hero.rect.x += 10
 
+        pygame.display.flip()
+        screen.fill((0,0,0))
+        hero.output_hero()
 
 start_game()
+       
