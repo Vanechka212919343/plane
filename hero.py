1ff05b18e0d3cdc4ea4a2fcf64227e4fd472c7fd
egor <troyanviskov@gmail.com>
2023-11-03 21:24:15 +0300

    update:new hero and correction

--- hero.py
+++ hero.py
@@ -1,25 +1,29 @@
 import pygame
+import sys
 
-class Hero():
+
+class Hero:
     def __init__(self, screen):
         self.image = pygame.image.load("images/hero.png")
         self.rect = self.image.get_rect()
         self.screen = screen
         self.screen_rect = screen.get_rect()
         self.rect.bottom = self.screen_rect.bottom
-        self.rect.x = self.screen_rect.centerx
-        self.move_right = False
-        self.move_left = False
+        self.rect.centerx = self.screen_rect.centerx
+        self.speed = 5
+        self.moving_right = False
+        self.moving_left = False
 
-        
+    def update(self):
+        if self.moving_right:
+            self.rect.centerx += self.speed
+        if self.moving_left:
+            self.rect.centerx -= self.speed
+            
+        if self.rect.left < self.screen_rect.left:
+            self.rect.left = self.screen_rect.left
+        if self.rect.right > self.screen_rect.right:
+            self.rect.right = self.screen_rect.right
+
     def output_hero(self):
         self.screen.blit(self.image, self.rect)
-    
-
-    def update_moving(self):
-        if self.move_right and self.rect.right < self.screen_rect.right:
-            self.rect.centerx += 1
-        if self.move_left and self.rect.left > self.screen_rect.left:
-            self.rect.centerx -= 1
-        
-
--- main.py
+++ main.py
@@ -3,15 +3,14 @@ import sys
 from hero import Hero
 
 def start_game():
-    #основна функция для описания игры
+    '''основная функция для описания игры'''
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption( "Space Invaders")
+    pygame.display.set_caption("Space Invaders")
+
     clock = pygame.time.Clock()
+    FPS = 60
 
-
-    
-    #объекты классов
     hero = Hero(screen)
 
     flag = True
@@ -19,23 +18,22 @@ def start_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    hero.move_left = True
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_d:
-                    hero.move_right = True
+                    hero.moving_right = True
                 if event.key == pygame.K_a:
-                    hero.move_left = False
+                    hero.moving_left = True
+            if event.type == pygame.KEYUP:
                 if event.key == pygame.K_d:
-                    hero.move_right = False
-        
-            
+                    hero.moving_right = False
+                if event.key == pygame.K_a:
+                    hero.moving_left = False
+
+        hero.update()
 
         screen.fill(0)
         hero.output_hero()
-        pygame.display.flip() 
-        hero.update_moving()
-        
 
+        pygame.display.flip()
+        clock.tick(FPS)
 start_game()
-       
