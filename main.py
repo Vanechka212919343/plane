32a6974b14fee350d4ffcb3db34f431a99e54b17
egor <troyanviskov@gmail.com>
2023-11-09 20:14:11 +0300

    update: all

--- main.py
+++ main.py
@@ -5,26 +5,28 @@ from pygame.sprite import Group
 def start_game():
     '''основная функция для описания игры'''
     pygame.init()
-    screen = pygame.display.set_mode((700, 1000))
+    screen = pygame.display.set_mode((600, 800))
     pygame.display.set_caption("Space Invaders")
     clock = pygame.time.Clock()
-    background_image = pygame.image.load('images/background.jpg')
+    #background_image = pygame.image.load('images/background.jpg')
 
     #объекты классов
     hero = Hero(screen)
     bullets = Group()
-    inosogradosmexicanos = Group()
+    enemys = Group()
 
     flag = True
     while flag:
-        screen.blit(background_image, (0, 0))
+       
+        controls.events(screen, hero, bullets)
+        hero.moving_hero(screen)
+        controls.update(screen, hero, bullets, enemys)
+        controls.moving_bullets(screen, bullets)
+        controls.create_army(screen, enemys)
+
+        #screen.blit(background_image, (0, 0))
         pygame.display.update()
         clock.tick(60)
-        controls.events(screen, hero, bullets) 
-        hero.moving_hero(screen)
-        controls.update(screen, hero, bullets)
-        controls.update_bullets(screen, hero, bullets)
-        controls.create_army(screen)
-       
- 
+
+        
 start_game()
